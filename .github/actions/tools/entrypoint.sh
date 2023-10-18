f0507ef908199d6b3e0cd4683e73f40badcf8eca
David Bauer <mail@david-bauer.net>
2023-10-18 09:21:42 +0200

    github: add host-tools build action

---
+++ .github/actions/tools/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[ -n "$GLUON_REPO" ] || GLUON_REPO="/gluon/gluon-repo"
+[ -n "$GLUON_ARTIFACT_DIR" ] || GLUON_ARTIFACT_DIR="/gluon/artifacts"
+
+OPENWRT_DIR="$GLUON_REPO/openwrt"
+OPENWRT_LUA_TARGET="openwrt/staging_dir/hostpkg/bin/lua"
+
+ln -s /gluon/site-repo "$GLUON_REPO/site"
+
+# Create manifest
+make -C "$GLUON_REPO" update
+
+# Compile Lua
+make -C "$GLUON_REPO" "-j$(nproc)" V=s "openwrt/staging_dir/hostpkg/bin/lua"
+
+for branch in $GLUON_MANIFEST_BRANCHES; do
+	make -C "$GLUON_REPO" manifest \
+		"-j$(nproc)" \
+		"V=s" \
+		"BROKEN=1" \
+		"GLUON_RELEASE=$GLUON_RELEASE" \
+		"GLUON_AUTOUPDATER_BRANCH=$branch" \
+		"GLUON_PRIORITY=$GLUON_PRIORITY" \
+		"GLUON_IMAGEDIR=$GLUON_ARTIFACT_DIR/output/images"
+done
+
+# Pack output
+tar czf "$GLUON_ARTIFACT_DIR/staging_dir.tar.gz" -C "$OPENWRT_DIR" staging_dir
