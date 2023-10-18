3cc850df9cdfc5998c42dd54da1457220af9a264
David Bauer <mail@david-bauer.net>
2023-10-18 10:28:56 +0200

    github: use cache for host-tools

--- .github/actions/tools/entrypoint.sh
+++ .github/actions/tools/entrypoint.sh
@@ -4,6 +4,7 @@
 [ -n "$GLUON_ARTIFACT_DIR" ] || GLUON_ARTIFACT_DIR="/gluon/artifacts"
 
 OPENWRT_DIR="$GLUON_REPO/openwrt"
+OPENWRT_STAGING_DIR="$OPENWRT_DIR/staging_dir"
 OPENWRT_LUA_TARGET="openwrt/staging_dir/hostpkg/bin/lua"
 
 ln -s /gluon/site-repo "$GLUON_REPO/site"
@@ -26,4 +27,4 @@ for branch in $GLUON_MANIFEST_BRANCHES; do
 done
 
 # Pack output
-tar czf "$GLUON_ARTIFACT_DIR/staging_dir.tar.gz" -C "$OPENWRT_DIR" staging_dir
+tar cJf "$GLUON_ARTIFACT_DIR/openwrt-host-tools.tar.gz" -C "$OPENWRT_STAGING_DIR" host
