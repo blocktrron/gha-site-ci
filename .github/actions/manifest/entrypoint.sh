b56fdb0695c17fbdd4daf69666e4116bf37b0a86
David Bauer <mail@david-bauer.net>
2023-10-18 07:07:06 +0200

    github: use single artifact dir for manifest

--- .github/actions/manifest/entrypoint.sh
+++ .github/actions/manifest/entrypoint.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 [ -n "$GLUON_REPO" ] || GLUON_REPO="/gluon/gluon-repo"
-[ -n "$GLUON_ARTIFACT_INPUT_DIR" ] || GLUON_ARTIFACT_INPUT_DIR="/gluon/artifacts-input"
-[ -n "$GLUON_ARTIFACT_OUTPUT_DIR" ] || GLUON_ARTIFACT_OUTPUT_DIR="/gluon/artifacts-output"
+[ -n "$GLUON_ARTIFACT_DIR" ] || GLUON_ARTIFACT_DIR="/gluon/artifacts"
 [ -n "$GLUON_RELEASE" ] || error 1
 [ -n "$GLUON_PRIORITY" ] || GLUON_PRIORITY=1
 [ -n "$GLUON_MANIFEST_BRANCHES" ] || error 1
 
 ln -s /gluon/site-repo /gluon/gluon-repo/site
-mkdir "$GLUON_ARTIFACT_OUTPUT_DIR/output"
 
 # Parse list of targets to build manifest for
 PARSED_TARGET_LIST="$(cat /gluon/action-data/targets.list)"
 
 # Read target names to list
 ARTIFACT_NAMES=""
-for filename in $GLUON_ARTIFACT_INPUT_DIR/*/ ; do
+for filename in $GLUON_ARTIFACT_DIR/*/ ; do
 	artifact_folder_name="$(basename $filename)"
 	echo "Target artifact: ${artifact_folder_name} (Full: ${filename})"
 
@@ -23,20 +21,21 @@ for filename in $GLUON_ARTIFACT_INPUT_DIR/*/ ; do
 done
 
 # Combine artifacts
+mkdir "$GLUON_ARTIFACT_DIR/output"
 for artifact_target in $ARTIFACT_NAMES ; do
 	# Check if artifact in list. Only delete otherwise.
 	if [ -n "$PARSED_TARGET_LIST" ] && [[ "$PARSED_TARGET_LIST" =~ "$artifact_target" ]]; then
 		echo "Combining ${artifact_target}"
 		# Unpack archive
-		tar xf "${GLUON_ARTIFACT_INPUT_DIR}/${artifact_target}/output.tar.gz" -C "${GLUON_ARTIFACT_INPUT_DIR}/${artifact_target}"
-		rm "${GLUON_ARTIFACT_INPUT_DIR}/${artifact_target}/output.tar.gz"
+		tar xf "${GLUON_ARTIFACT_DIR}/${artifact_target}/output.tar.gz" -C "${GLUON_ARTIFACT_DIR}/${artifact_target}"
+		rm "${GLUON_ARTIFACT_DIR}/${artifact_target}/output.tar.gz"
 
 		# Combine targets
-		rsync -a ${GLUON_ARTIFACT_INPUT_DIR}/${artifact_target}/* "$GLUON_ARTIFACT_OUTPUT_DIR/output"
+		rsync -a ${GLUON_ARTIFACT_DIR}/${artifact_target}/* "$GLUON_ARTIFACT_DIR/output"
 	else
 		echo "Skipping ${artifact_target}"
 	fi
-	rm -rf "${GLUON_ARTIFACT_INPUT_DIR}/${artifact_target}"
+	rm -rf "${GLUON_ARTIFACT_DIR}/${artifact_target}"
 done
 
 # Create manifest
@@ -49,9 +48,9 @@ for branch in $GLUON_MANIFEST_BRANCHES; do
 		"GLUON_RELEASE=$GLUON_RELEASE" \
 		"GLUON_AUTOUPDATER_BRANCH=$branch" \
 		"GLUON_PRIORITY=$GLUON_PRIORITY" \
-		"GLUON_IMAGEDIR=$GLUON_ARTIFACT_OUTPUT_DIR/output/images"
+		"GLUON_IMAGEDIR=$GLUON_ARTIFACT_DIR/output/images"
 done
 
 # Pack output
-tar czf "$GLUON_ARTIFACT_OUTPUT_DIR/output.tar.gz" -C "$GLUON_ARTIFACT_OUTPUT_DIR" output
-rm -rf "$GLUON_ARTIFACT_OUTPUT_DIR/output"
+tar czf "$GLUON_ARTIFACT_DIR/output.tar.gz" -C "$GLUON_ARTIFACT_DIR" output
+rm -rf "$GLUON_ARTIFACT_DIR/output"
