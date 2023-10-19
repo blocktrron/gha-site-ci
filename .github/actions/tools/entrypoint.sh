f7291482b939ef9be64bbc4fb83812747790ceae
David Bauer <mail@david-bauer.net>
2023-10-19 21:24:05 +0200

    only pack staging and build dir

--- .github/actions/tools/entrypoint.sh
+++ .github/actions/tools/entrypoint.sh
@@ -4,7 +4,9 @@
 [ -n "$GLUON_ARTIFACT_DIR" ] || GLUON_ARTIFACT_DIR="/gluon/artifacts"
 
 OPENWRT_DIR="$GLUON_REPO/openwrt"
+OPENWRT_BUILD_DIR="$OPENWRT_DIR/build_dir"
 OPENWRT_STAGING_DIR="$OPENWRT_DIR/staging_dir"
+PACKING_STAGING_DIR="$RUNNER_TEMP/openwrt-tools"
 OPENWRT_LUA_TARGET="openwrt/staging_dir/hostpkg/bin/lua"
 
 ln -s /gluon/site-repo "$GLUON_REPO/site"
@@ -15,8 +17,10 @@ make -C "$GLUON_REPO" update
 # Compile Lua
 make -C "$GLUON_REPO" "-j$(nproc)" V=s "GLUON_AUTOREMOVE=1" "openwrt/staging_dir/hostpkg/bin/lua"
 
-# Clean OpenWrt Download Cache
-rm -rf "$OPENWRT_DIR/dl"
+# Move OpenWrt build and staging dir to packing staging dir
+mkdir -p $PACKING_STAGING_DIR
+mv $OPENWRT_BUILD_DIR $PACKING_STAGING_DIR
+mv $OPENWRT_STAGING_DIR $PACKING_STAGING_DIR
 
 # Pack output
-tar cJf "$GLUON_ARTIFACT_DIR/openwrt.tar.xz" -C "$GLUON_REPO" --posix openwrt
+tar cJf "$GLUON_ARTIFACT_DIR/openwrt.tar.xz" -C "$PACKING_STAGING_DIR" --posix staging_dir build_dir
