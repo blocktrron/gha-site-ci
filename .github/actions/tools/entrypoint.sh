c6f4f6ea05e8187951de0e977ffd27f814954d73
David Bauer <mail@david-bauer.net>
2023-11-07 22:03:29 +0100

    tools: cleanup

--- .github/actions/tools/entrypoint.sh
+++ .github/actions/tools/entrypoint.sh
@@ -2,21 +2,14 @@
 
 set -euxo pipefail
 
-[ -n "$GLUON_REPO" ] || GLUON_REPO="/gluon/gluon-repo"
-[ -n "$GLUON_ARTIFACT_DIR" ] || GLUON_ARTIFACT_DIR="/gluon/artifacts"
+GLUON_REPO="/gluon/gluon-repo"
+GLUON_ARTIFACT_DIR="/gluon/artifacts"
 
-OPENWRT_DIR="$GLUON_REPO/openwrt"
-OPENWRT_BUILD_DIR="$OPENWRT_DIR/build_dir"
-OPENWRT_STAGING_DIR="$OPENWRT_DIR/staging_dir"
-OPENWRT_LUA_TARGET="openwrt/staging_dir/hostpkg/bin/lua"
-
-ln -s /gluon/site-repo "$GLUON_REPO/site"
-
-# Create manifest
-make -C "$GLUON_REPO" update
+# Update repo
+make -C "$GLUON_REPO" "GLUON_SITEDIR=/gluon/site-repo" update
 
 # Compile Lua
-make -C "$GLUON_REPO" "-j$(nproc)" V=s "GLUON_AUTOREMOVE=1" "openwrt/staging_dir/hostpkg/bin/lua"
+make -C "$GLUON_REPO" "-j$(nproc)" V=s "GLUON_AUTOREMOVE=1" "GLUON_SITEDIR=/gluon/site-repo" "openwrt/staging_dir/hostpkg/bin/lua"
 
 # Pack output
 tar cJf "$GLUON_ARTIFACT_DIR/openwrt.tar.xz" --posix -C "$GLUON_REPO" openwrt
