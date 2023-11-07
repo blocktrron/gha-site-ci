71e63db7901c037ee97b0176da5a6e3a7ee2767d
David Bauer <mail@david-bauer.net>
2023-11-07 21:42:43 +0100

    get-targets: remove argument

--- .github/actions/get-targets/entrypoint.sh
+++ .github/actions/get-targets/entrypoint.sh
@@ -3,7 +3,6 @@
 set -euxo pipefail
 
 GLUON_PATH="$1"
-OUTPUT_FILE="$2"
 
 # Get Target list
 make --no-print-directory -C $GLUON_PATH list-targets "BROKEN=${BROKEN}" "GLUON_SITEDIR=$GLUON_PATH/docs/site-example" | jq  --raw-input .  | jq --slurp . | jq -c .
