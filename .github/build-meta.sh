d6b8f547db000dbb63ad07e7ec78dad1b8720930
David Bauer <mail@david-bauer.net>
2023-11-08 23:31:23 +0100

    cleanup

--- .github/build-meta.sh
+++ .github/build-meta.sh
@@ -4,8 +4,11 @@ set -euxo pipefail
 
 SCRIPT_DIR="$(dirname "$0")"
 
-# Build BROKEN by default. Disable for release builds
+# Build BROKEN by default. Disable for release builds.
 BROKEN="1"
+
+# Don't deploy by default. Enable for release and testing builds.
+DEPLOY="0"
 
 # Release Branch regex
 RELEASE_BRANCH_RE="v[2-9]\.[0-9]\.x"
@@ -21,9 +24,10 @@ GLUON_COMMIT="$(jq -r -e .gluon.commit "$SCRIPT_DIR/build-info.json")"
 # Get Container version information
 CONTAINER_VERSION="$(jq -r -e .container.version "$SCRIPT_DIR/build-info.json")"
 
-DEPLOY=0
+# Get Default Release version from site.mk
 DEFAULT_RELEASE_VERSION="$(make --no-print-directory -C $SCRIPT_DIR -f ci-build.mk version)"
 
+# Enable Manifest generation conditionally
 MANIFEST_STABLE="0"
 MANIFEST_BETA="0"
 MANIFEST_TESTING="0"
@@ -33,48 +37,48 @@ echo "GitHub Ref-Name: $GITHUB_REF_NAME"
 
 # Determine Autoupdater Branch to use
 if [ "$GITHUB_REF_TYPE" = "branch" ]; then
-	# Don't generate manifest on push
-	MANIFEST_BRANCHES=""
-
-	# HACKHACKHACK - Don't skip manifest branches
-	MANIFEST_BRANCHES="stable beta"
-
 	if [ "$GITHUB_REF_NAME" = "master" ]; then
 		# Push to master - autoupdater Branch is testing and enabled
-		AUTOUPDATER_ENABLED=1
+		AUTOUPDATER_ENABLED="1"
 		AUTOUPDATER_BRANCH="testing"
+
 		MANIFEST_TESTING="1"
 	elif [[ "$GITHUB_REF_NAME" =~ $RELEASE_BRANCH_RE ]]; then
 		# Push to release branch - autoupdater Branch is stable and enabled
-		AUTOUPDATER_ENABLED=1
+		AUTOUPDATER_ENABLED="1"
 		AUTOUPDATER_BRANCH="stable"
+
 		MANIFEST_STABLE="1"
 		MANIFEST_BETA="1"
 	else
 		# Push to unknown branch - Disable autoupdater
-		AUTOUPDATER_ENABLED=0
+		AUTOUPDATER_ENABLED="0"
 		AUTOUPDATER_BRANCH="testing"
 	fi
 elif [ "$GITHUB_REF_TYPE" = "tag" ]; then
 	if [[ "$GITHUB_REF_NAME" =~ $TESTING_TAG_RE ]]; then
 		# Testing release - autoupdater Branch is testing and enabled
-		AUTOUPDATER_ENABLED=1
+		AUTOUPDATER_ENABLED="1"
 		AUTOUPDATER_BRANCH="testing"
+
 		MANIFEST_TESTING="1"
+
 		RELEASE_VERSION="$(echo "$GITHUB_REF_NAME" | tr '-' '~')"
-		DEPLOY=1
+		DEPLOY="1"
 	elif [[ "$GITHUB_REF_NAME" =~ $RELEASE_TAG_RE ]]; then
 		# Stable release - autoupdater Branch is stable and enabled
-		AUTOUPDATER_ENABLED=1
+		AUTOUPDATER_ENABLED="1"
 		AUTOUPDATER_BRANCH="stable"
+
 		MANIFEST_STABLE="1"
 		MANIFEST_BETA="1"
+
 		RELEASE_VERSION="$GITHUB_REF_NAME"
 		BROKEN="0"
-		DEPLOY=1
+		DEPLOY="1"
 	else
 		# Unknown release - Disable autoupdater
-		AUTOUPDATER_ENABLED=0
+		AUTOUPDATER_ENABLED="0"
 		AUTOUPDATER_BRANCH="testing"
 	fi
 else
