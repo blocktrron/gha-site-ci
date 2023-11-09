20f430d073ab825e9375c18cc7574a4304d6bc7f
David Bauer <mail@david-bauer.net>
2023-11-09 21:45:59 +0100

    create release script

---
+++ .github/create-release.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$0")"
+
+RELEASE_NAME="${1-}"
+
+# Regex for testing firmware tag
+TESTING_TAG_RE="^[2-9].[0-9]~[0-9]{8}$"
+# Regex for release firmware tag
+RELEASE_TAG_RE="^[2-9].[0-9].[0-9]$"
+
+# Check if we have an argument provided.
+if [ -n "$RELEASE_NAME" ]; then
+    if [[ "$RELEASE_NAME" =~ $RELEASE_TAG_RE ]] || [[ "$RELEASE_NAME" =~ $TESTING_TAG_RE ]]; then
+        # Success
+        echo "Provided Release Name '$RELEASE_NAME' is valid"
+    else
+        # Failure
+        echo "Provided Release Name '$RELEASE_NAME' is invalid"
+        exit 1
+    fi
+else
+    RELEASE_NAME="$(make --no-print-directory -C $SCRIPT_DIR -f ci-build.mk version)"
+fi
+
+echo "Proceed to tag firmware release for '$RELEASE_NAME'? (y/n)"
+
+read -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Proceeding to tag firmware release with $RELEASE_NAME"
+else
+    echo "Aborting"
+    exit 1
+fi
+
+git tag "$RELEASE_NAME"
+
+echo "Tag was created"
