2ee5df1d2c4b560bbc83f4a8aa197a30f9f68ba0
David Bauer <mail@david-bauer.net>
2023-11-20 17:50:54 +0100

    actions: remove downstream signing action

--- .github/actions/sign/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-SIGNING_KEY_PATH="/gluon/signing-key/signing.key"
-GLUON_DIR="/gluon/gluon-repo"
-MANIFEST_PATH="/gluon/output-dir/images/sysupgrade/$ACTION_MANIFEST.manifest"
-
-$GLUON_DIR/contrib/sign.sh "$SIGNING_KEY_PATH" "$MANIFEST_PATH"
